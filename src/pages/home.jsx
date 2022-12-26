986260c92bb24f980dda27d6e5df6973119bde7c
Manish Parmar <m.p982525@gmail.com>
2022-12-26 21:12:29 +0530

    Initial Commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import { useLocation } from "react-router";
+
+const Home = () => {
+
+    // const location = useLocation(); 
+    // useEffect(() => {
+    //   // Scroll top when location changes
+    //   window.scrollTo(0, 0);
+    // }, [location]);
+  
+    return (
+      <div class="App">
+       <h3>This is Home Page</h3>
+      </div>
+  );
+};
+
+export default Home;
